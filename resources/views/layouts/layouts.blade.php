37e443b1050cd2d4c1ec1e2b0f04a25a7920827a
Wahyu <97522347+SiWahyu@users.noreply.github.com>
2023-01-26 13:20:40 +0700

    membuat form login dengan laravel

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -16,5 +16,13 @@
         @yield('contact')
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM=" crossorigin="anonymous"></script>
+    <script>
+        $(function() {
+            setTimeout(() => {
+                $(".alert").fadeOut();
+            }, 3000);
+        });
+    </script>
   </body>
 </html>
